0320fc456a9b0b1c91e0625b543e2c0053a0554e
korey ko <koreykendrick1@gmail.com>
2020-06-30 21:59:41 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_06_29_224652) do
 
-  create_table "create_movies", force: :cascade do |t|
+  create_table "movies", force: :cascade do |t|
     t.string "title"
     t.integer "release_date"
     t.string "director"
